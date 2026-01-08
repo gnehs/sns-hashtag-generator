fe71a27d22960129bbbe4d7fbad10382c89b8288
gnehs <gnehs@gnehs.net>
2026-01-08 14:45:04 +0800

    chore: rebrand to SNS hashtag generator

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,10 @@
 <template>
   <footer class="container">
-    Made with <a href="https://pancake.tw" target="_blank">🥞</a> in Taiwan | Developed by <a href="https://gnehs.net" target="_blank">gnehs</a>
-    | <a href="https://github.com/gnehs/instagram-hashtag-generator" target="_blank">GitHub</a>
+    Made with <a href="https://pancake.tw" target="_blank">🥞</a> in Taiwan |
+    Developed by <a href="https://gnehs.net" target="_blank">gnehs</a> |
+    <a href="https://github.com/gnehs/SNS-hashtag-generator" target="_blank"
+      >GitHub</a
+    >
   </footer>
 </template>
 <style lang="sass" scoped>
--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,50 @@
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      includeAssets: ['icon.svg'],
-      registerType: 'autoUpdate',
+      includeAssets: ["icon.svg"],
+      registerType: "autoUpdate",
       manifest: {
-        name: 'Instagram 標籤小精靈',
-        short_name: 'Instagram 標籤小精靈',
-        description: '讓小精靈自動推薦給你適合的標籤',
-        theme_color: '#f2f2f2',
+        name: "SNS 標籤小精靈",
+        short_name: "SNS 標籤小精靈",
+        description: "讓小精靈自動推薦給你適合的標籤",
+        theme_color: "#f2f2f2",
         icons: [
           {
-            "src": "/icon/manifest-icon-192.maskable.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any"
+            src: "/icon/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
           },
           {
-            "src": "/icon/manifest-icon-192.maskable.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
+            src: "/icon/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
           },
           {
-            "src": "/icon/manifest-icon-512.maskable.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "any"
+            src: "/icon/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
           },
           {
-            "src": "/icon/manifest-icon-512.maskable.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
-          }
+            src: "/icon/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
         ],
-        lang: 'zh-TW',
+        lang: "zh-TW",
       },
-    })
+    }),
   ],
   alias: {
-    '@': '/src'
-  }
-})
+    "@": "/src",
+  },
+});
